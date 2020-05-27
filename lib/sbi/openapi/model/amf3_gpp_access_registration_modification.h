bade0ff8d320db80d861affc2ac9a73066803139
Sukchan Lee <acetcom@gmail.com>
2020-05-27 12:52:27 -0400

    update it

--- lib/sbi/openapi/model/amf3_gpp_access_registration_modification.h
+++ lib/sbi/openapi/model/amf3_gpp_access_registration_modification.h
@@ -13,6 +13,7 @@
 #include "../include/keyValuePair.h"
 #include "../include/binary.h"
 #include "backup_amf_info.h"
+#include "eps_interworking_info.h"
 #include "guami.h"
 #include "ims_vo_ps.h"
 
@@ -27,6 +28,7 @@ typedef struct OpenAPI_amf3_gpp_access_registration_modification_s {
     char *pei;
     struct OpenAPI_ims_vo_ps_s *ims_vo_ps;
     OpenAPI_list_t *backup_amf_info;
+    struct OpenAPI_eps_interworking_info_s *eps_interworking_info;
     int ue_srvcc_capability;
 } OpenAPI_amf3_gpp_access_registration_modification_t;
 
@@ -36,6 +38,7 @@ OpenAPI_amf3_gpp_access_registration_modification_t *OpenAPI_amf3_gpp_access_reg
     char *pei,
     OpenAPI_ims_vo_ps_t *ims_vo_ps,
     OpenAPI_list_t *backup_amf_info,
+    OpenAPI_eps_interworking_info_t *eps_interworking_info,
     int ue_srvcc_capability
     );
 void OpenAPI_amf3_gpp_access_registration_modification_free(OpenAPI_amf3_gpp_access_registration_modification_t *amf3_gpp_access_registration_modification);
